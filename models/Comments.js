d1723d821bc0b5dc2e7dfffbf3e6c87326766184
octavio <serrayon81@gmail.com>
2019-07-23 17:15:09 -0700

    adds functionality to User Comments

--- models/Comments.js
+++ models/Comments.js
@@ -3,7 +3,17 @@ const Schema = mongoose.Schema;
 
 const CommentsSchema = new Schema({
   name: {type: String},
-  message: {type: String}
+  message: {type: String},
+  book: 
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Books' 
+    },
+  user: 
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Users' 
+    }
 });
 
 const Comments = mongoose.model('Comments', CommentsSchema);
