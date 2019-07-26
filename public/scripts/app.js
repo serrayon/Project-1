1b9bb315fa123303da455019a3abddf0f1337738
octavio <serrayon81@gmail.com>
2019-07-25 18:56:05 -0700

    merge conflict fix

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,11 @@
 console.log("its working");
 
-//-----------global variables-------------------//
+//-----------global constiables-------------------//
 const BASE_URL = '/api/v1/books';
 const COMMENTS_URL = '/api/v1/comments';
+//make 
 
-// -------------------------------- GLOBAL VARIABLES ------------------------------- //
+// -------------------------------- GLOBAL constIABLES ------------------------------- //
 let books = []
 let books_template = null;
 
@@ -13,29 +14,29 @@ function renderBooks(books) {
   books.forEach((book) => {
     $('.books').append(bookTemplate(book))
   })
-}
-
-// render book
-function renderBook(book) {
+ }
+ 
+ // render book
+ function renderBook(book) {
   let html = showBookTemplate(book, [{message: "good book", user: {username: 'Zafar'}}])
   $('.book').append(html)
-}
-
-//render comments
-
-function renderComments(comments, book_id) {
+ }
+ 
+ //render comments
+ 
+ function renderComments(comments, book_id) {
   comments.forEach((comment) => {
     $('.comments').append(commentTemplate(comment))
   })
-
+ 
   if (book_id) {
     $(".comment-wrapper").append(newCommentTemplateForm(book_id))
     $('.show-book').append($("<a href='/main'>Go back</a>"))
   }
-}
-
-// Book template
-const bookTemplate = (book) => {
+ }
+ 
+ // Book template
+ const bookTemplate = (book) => {
     return `
         <div id='${book._id}'>
             <h4>${book.title}</h4>
@@ -43,14 +44,14 @@ const bookTemplate = (book) => {
             <p class='author_name'>${book.author_name}</p>
             <img class='author_photo' src=${book.author_photo} />
             <p class='author_location'>${book.author_location}</p>
-            <button class='delete-button'>&times;</button>
-            <button class='edit-button'>edit</button>
-            <button class='show-button'>show</button>
+            <button class='delete-button'>delete</button>
+          
+            <button class='show-button'>comment</button>
         </div>
     `;
-};
-
-const showBookTemplate = (book) => {
+ };
+ 
+ const showBookTemplate = (book) => {
     return `
         <div id='${book._id}'>
             <h4>${book.title}</h4>
@@ -58,13 +59,13 @@ const showBookTemplate = (book) => {
             <p class='author_name'>${book.author_name}</p>
             <img class='author_photo' src=${book.author_photo} />
             <p class='author_location'>${book.author_location}</p>
-
+ 
         </div>
     `;
-};
-
-// Comment template
-const newCommentTemplateForm = (book_id) => {
+ };
+ 
+ // Comment template
+ const newCommentTemplateForm = (book_id) => {
   return `
     <h1>New Comment</h1>
     <form action='/api/v1/comments' method='post' class='new-comment'>
@@ -74,22 +75,27 @@ const newCommentTemplateForm = (book_id) => {
             <button type="submit">Add</button>
     </form>
   `
-}
-
-const commentTemplate = (comment) => {
+ }
+ 
+ const commentTemplate = (comment) => {
     return `
         <div id='${comment._id}'>
             <h4>${comment.message}</h4>
+            <button class='delete-button'>&times;</button>
+            <button class='edit-button'>edit</button>
         </div>
     `;
-};
+ };
 
-$(document).ready(() => {
+ 
+
+
+ $(document).ready(() => {
   $('.books').on('click', '.delete-button', function() {
-    var parent = $(this).parent()
-
+    const parent = $(this).parent()
+ 
     let book_id = parent.attr('id')
-
+ 
     // Delet single book
     $.ajax({
       url: `${BASE_URL}/${book_id}`,
@@ -99,17 +105,17 @@ $(document).ready(() => {
         $(`#${book_id}`).remove()
       },
       failure: function() {
-
+ 
       }
     })
   })
-
+ 
   $('.books').on('click', '.show-button', function() {
-    var parent = $(this).parent()
-
+    const parent = $(this).parent()
+ 
     let book_id = parent.attr('id')
-
-
+ 
+ 
  // get single book
     $.ajax({
       url: `${BASE_URL}/${book_id}`,
@@ -120,14 +126,14 @@ $(document).ready(() => {
         $('.new-book').hide()
         renderBook(response.data)
         $('.show-book').show()
-
+ 
       },
       failure: function() {
-
+ 
       }
     })
-
-
+ 
+ 
  // get comments for book
     $.ajax({
       url: `${COMMENTS_URL}`,
@@ -140,13 +146,13 @@ $(document).ready(() => {
         renderComments(response.data, book_id)
       },
       failure: function() {
-
+ 
       }
     })
   })
-
+ 
   // fetch all books
-
+ 
   $.ajax({
     url: BASE_URL,
     method: 'GET',
@@ -155,15 +161,15 @@ $(document).ready(() => {
       renderBooks(response.data)
     },
     failure: function() {
-
+ 
     }
   })
-
-
+ 
+ 
  // New book create
   $('.new-book').on('submit', function(event) {
     const form = $(this);
-
+ 
     $.ajax({
       url: BASE_URL,
       method: 'POST',
@@ -173,18 +179,18 @@ $(document).ready(() => {
         renderBooks([book])
       },
       failure: function() {
-
+ 
       }
     })
-
+ 
     return false;
   })
-
+ 
   // New comment create for book
-
-  $(document).delegate('.new-comment', 'submit', function(event) {
+ 
+  $(document).on('submit', '.new-comment', function(event) {
     const form = $(this);
-
+ 
     $.ajax({
       url: COMMENTS_URL,
       method: 'POST',
@@ -194,41 +200,99 @@ $(document).ready(() => {
         renderComments([comment])
       },
       failure: function() {
-
+ 
       }
     })
-
+ 
     event.preventDefault()
-
-    return false;
+ 
+    //return false;
   })
-})
+ 
+  $('.comments').on('click', '.delete-button', function() {
+    const parent = $(this).parent()
+ 
+    let comment_id = parent.attr('id')
+ console.log(`i am a delete`);
+    // Delete single comment
+    $.ajax({
+      url: `${BASE_URL}/${comment_id}`,
+      method: 'DELETE',
+      dataType: 'JSON',
+      success: function(response) {
+        $(`#${comment_id}`).remove()
+      },
+      failure: function() {
+ 
+      }
+    })
+  });
+
+  $('.comments').on('click', '.edit-button', function() {
+    const parent = $(this).parent()
+   console.log(`i am a update `);
+    let comment_id = parent.attr('id')
+ 
+    // edit single comment
+
+$.ajax(`${BASE_URL}/${comment_id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        const handleClick = (event) => {
+          event.preventDefault();
+          if (event.target.classList.contains('edit-button')) {
+            editComment(event);
+          } else if (event.target.classList.contains('submit-edit')) {
+            updateComment(event);
 
 
 
 
 
-// -------------------------------- STATE VARIABLES ------------------------------- //
+  });
+});
+    // const updateCity = (event) => {
+    //   const commentId = event.${BASE_URL}/${comment_id}
+    //     console.log(cityId);
+    //      const $cityId = $(event.target).parent().attr('id');
+    //      const $cityDescription = $('#editCityDescription').val();
+      //   const cityName = document.getElementById('editCityName').value;
+      //   const cityDescription = document.getElementById('editCityDescription').value;
+      //   const newCity = { name: cityName, description: cityDescription };
+      //   console.log(newCity);
+      //   $.ajax(`${BASE_URL}/${cityId}`, {
+      //     method: 'PUT',
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //       const handleClick = (event) => {
+      //          if (event.target.classList.contains('edit-button')) {
+      //     editCity(event);
+      //      } else if (event.target.classList.contains('submit-edit')) {
+      //     updateCity(event);
+      //   }
+       //}
 
 
+    // $.ajax(`${BASE_URL}/${comment_id}`,{
+    //   method: 'PUT',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     const handleClick = (event) => {
+    //       event.preventDefault();
+    //       if (event.target.classList.contains('edit-button')) {
+    //         editComment(event);
+    //       } else if (event.target.classList.contains('submit-edit')) {
+    //         updateComment(event);
+    //       }
+    //     } 
+ 
+    //   }
+    // })
+  
 
 
-
-
-
-// -------------------------------- DOM ELEMENTS ------------------------------- //
-
-
-
-
-
-
-// -------------------------------- FUNCTIONS ------------------------------- //
-
-
-
-
-
-//--------------Event Listeners-----------------------//
-
-// $citiesSection.on('click', deleteComment);
+//  success: function(response) {
+//   $(`#${comment_id}`)
+// },
+// failure: function() {
--- routes/comments.js
+++ routes/comments.js
@@ -69,4 +69,19 @@ router.delete('/:comment_id', (req, res) => {
     });
   });
 
+// Update 
+
+router.put('/:comment_id', (req, res) => {
+  db.Comment.findByIdAndUpdate(req.params.comment_id, (err, updateComment) => {
+    if (err) return res.status(400).json({
+      status: 400,
+      message: 'Something went wrong, please try again',
+    });
+    console.log(deletedComment);
+      res.status(200).json({
+        status: 200,
+        message: 'Success',
+      });
+  });
+});
   module.exports = router;
